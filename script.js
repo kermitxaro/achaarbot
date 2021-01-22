b5c7de5ea2d8dbec08f8fe1ddf8202042910e126
kermitxaro <77839387+kermitxaro@users.noreply.github.com>
2021-01-22 20:41:59 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.typed', {
+  strings: ["/help", "تیم برنامه نویسی آچــار", "بات فارسی زبان دیسکورد", "بهترین شکل", "بهترین قابلیت ها", "استفاده آسان", "داشبوردی جذاب"],
+  typeSpeed: 80,
+  backSpeed: 70,
+  loop: true,
+  shuffle: true,
+  backDelay: 3000,
+  contentType: 'html'
+});
